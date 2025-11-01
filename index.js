0a688c44a78ec4b34bdbcbe7575a1880cecf13cd
melvisrmoba <melvis.r.moba@gmail.com>
2025-11-01 16:08:42 -0500

    Delete All functionality added

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 // get leads stored in LocalStorage and converts  back to array (parse)
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
+//clears localstorage, myleads and the dom when the delete button is double clicked
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads(); // since the array is empty, just call the renderLeads function
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
