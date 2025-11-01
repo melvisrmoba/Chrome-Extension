045532b50d132e84922e66bfc21b2920ac021752
melvisrmoba <melvis.r.moba@gmail.com>
2025-11-01 17:13:57 -0500

    save tab button and functionality added

--- index.js
+++ index.js
@@ -5,35 +5,46 @@ const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 // get leads stored in LocalStorage and converts  back to array (parse)
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
-//clears localstorage, myleads and the dom when the delete button is double clicked
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads(); // since the array is empty, just call the renderLeads function
+
+tabBtn.addEventListener("click", function () {
+  //grab url of the current chrome tab, using the chrome tabs Api
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // converts the array into String and store in LocalStorage
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `;
   }
   ulEl.innerHTML = listItems;
 }
+
+//clears localstorage, myleads and the dom when the delete button is double clicked
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads); // since the array is empty, just call the renderLeads function
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // converts the array into String and store in LocalStorage
+  render(myLeads);
+});
